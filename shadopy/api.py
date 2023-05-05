3343adb5a612623b9424d2dc7ee565e8951ad51c
david <david@inceptive.tech>
2023-05-05 18:31:08 +0200

    added context information API usage

--- shadopy/api.py
+++ shadopy/api.py
@@ -8,13 +8,14 @@ from urllib.parse import urljoin
 from requests import Request, Session
 
 from shadopy.connexion import BaseConnexion, HttpBasicConnexion
-from shadopy.models import BlockDevices
+from shadopy.models import BlockDevices, ContextInformation, UserInfo, DataCenter, SkuInfo
 
 
 class ShadowCloudCli:
     """wrapps the shadow cloud public APIs"""
     BASE_URL = "https://api.shdw-ws.fr"
     BLOCK_DEVICE_LIST_PATH = "/api/block_device/list"
+    CTX_INFORMATION_PATH = "/api/context/informations"
 
     def __init__(self, connexion: BaseConnexion = None) -> None:
         super().__init__()
@@ -31,5 +32,13 @@ class ShadowCloudCli:
 
     def get_block_device_list(self, block_uuid_filters: List[str] = None) -> BlockDevices:
         # todo use filters
-        serialized_blocks = self._build_and_send_request(self.BLOCK_DEVICE_LIST_PATH, {})
+        data = {} if block_uuid_filters is None else {"filters": [{"uuid": elt} for elt in block_uuid_filters]}
+        serialized_blocks = self._build_and_send_request(self.BLOCK_DEVICE_LIST_PATH, data)
         return BlockDevices.parse_obj(serialized_blocks.json())
+
+    def get_context_information(self) -> ContextInformation:
+        serialized = self._build_and_send_request(self.CTX_INFORMATION_PATH, {}).json()
+        user = UserInfo.parse_obj(serialized["user"])
+        datacenters = [DataCenter(**value) for _, value in serialized["datacenters"].items()]
+        skus = [SkuInfo(**value) for _, value in serialized["vm"]["skus"].items()]
+        return ContextInformation(datacenters=datacenters, user=user, skus=skus)
