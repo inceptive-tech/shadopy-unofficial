d69bac16d1b86519f0f47517f89bb79e40e78342
david <david@inceptive.tech>
2023-05-05 18:31:25 +0200

    added unit tests on context information API

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,7 @@ from unittest.mock import patch
 
 from shadopy import ShadowCloudCli, BlockDevices, BlockDevice
 from shadopy.connexion import HttpBasicConnexion
+from shadopy.models import DataCenter, SkuInfo, UserInfo
 from tests import ResMock
 
 _BLOCK_TEST0 = {
@@ -25,11 +26,87 @@ _BLOCK_TEST0 = {
 }
 _BLOCK_LIST_TEST0 = {"block_devices": [_BLOCK_TEST0]}
 
+_SKU_INFO_TEST0 = {
+    "extra_core_cost": 15,
+    "extra_gpu_cost": 50,
+    "extra_ram_cost": 25,
+    "hv_cpu_count": 2,
+    "hv_cpu_ref": "2678v3",
+    "hv_gpu_count": 4,
+    "hv_gpu_ref": "p5000",
+    "hv_ram": 64,
+    "vm_core": 4,
+    "vm_core_max": 16,
+    "vm_cost": 100,
+    "vm_gpu": 1,
+    "vm_ram": 12,
+    "vm_ram_max": 48,
+    "vm_sku": "VM-P5000-2678V3-R2"
+}
+_USER_INFO_TEST0 = {
+    "max_block_device": 1,
+    "max_instances": 1,
+    "max_request_timeout": 300,
+    "superuser_level": 0,
+    "vm_max_uptime": 5400
+}
+_CTX_TEST0 = {
+    "datacenters": {
+        "SK1": {
+            "areas": [
+                "South-Korea",
+                "APAC"
+            ],
+            "label": "SK1"
+        },
+        "USSFO01": {
+            "areas": [
+                "North-America",
+                "USA",
+                "California"
+            ],
+            "label": "USSFO01"
+        }
+    },
+    "user": _USER_INFO_TEST0,
+    "vm": {
+        "skus": {
+            "VM-P5000-2678V3-R2": _SKU_INFO_TEST0,
+            "VM-RTX4000-7513-R1": {
+                "extra_core_cost": 23,
+                "extra_gpu_cost": 78,
+                "extra_ram_cost": 39,
+                "hv_cpu_count": 1,
+                "hv_cpu_ref": "7513",
+                "hv_gpu_count": 8,
+                "hv_gpu_ref": "rtx4000",
+                "hv_ram": 128,
+                "vm_core": 4,
+                "vm_core_max": 32,
+                "vm_cost": 155,
+                "vm_gpu": 1,
+                "vm_ram": 12,
+                "vm_ram_max": 96,
+                "vm_sku": "VM-RTX4000-7513-R1"
+            }
+        }
+    }
+}
 
-@patch("requests.sessions.Session.send", side_effect=[ResMock(_BLOCK_LIST_TEST0)])
+
 class TestShadowCloudCli(TestCase):
+    @patch("requests.sessions.Session.send", side_effect=[ResMock(_BLOCK_LIST_TEST0)])
     def test_get_block_list_nominal(self, _):
         api = ShadowCloudCli(HttpBasicConnexion("test", "test"))
         result = api.get_block_device_list()
         expected = BlockDevices(block_devices=[BlockDevice(**_BLOCK_TEST0)])
         self.assertEqual(expected, result)
+
+    @patch("requests.sessions.Session.send", side_effect=[ResMock(_CTX_TEST0)])
+    def test_get_ctx_info_nominal(self, _):
+        api = ShadowCloudCli(HttpBasicConnexion("test", "test"))
+        result = api.get_context_information()
+        self.assertIn(DataCenter(label="SK1", areas=["South-Korea", "APAC"]), result.datacenters)
+        self.assertIn(SkuInfo(**_SKU_INFO_TEST0), result.skus)
+        self.assertEqual(UserInfo(**_USER_INFO_TEST0), result.user)
+
