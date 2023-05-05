f07b738091f816bae6135d3becc9b09d54c7a533
david <david@inceptive.tech>
2023-05-05 17:29:28 +0200

    models.py : input / output models of the "block device list" API

---
+++ shadopy/models.py
@@ -0,0 +1,25 @@
+"""
+    input and output models of the shadow cloud API
+    David G. - Inceptive - 2022
+"""
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class BlockDevice(BaseModel):
+    allocated_on: str
+    cost: int
+    datacenter_label: str
+    id: str
+    inserted_on: str
+    mounted: bool  # named 'mount' in doc
+    released_on: Optional[str]
+    size_gib: int
+    status: int
+    status_str: str
+    uuid: str
+
+
+class BlockDevices(BaseModel):
+    block_devices: List[BlockDevice]
