2d1c22bb5973d7f8fc22ec84b9640cbcd6b4a253
david <david@inceptive.tech>
2023-05-05 18:30:54 +0200

    added context information api models

--- shadopy/models.py
+++ shadopy/models.py
@@ -26,6 +26,37 @@ class BlockDevices(BaseModel):
 
 
 class DataCenter(BaseModel):
-    name: str
     areas: List[str]
     label: str
+
+
+class UserInfo(BaseModel):
+    max_block_device: int
+    max_instances: int
+    max_request_timeout: int
+    superuser_level: int
+    vm_max_uptime: int
+
+
+class SkuInfo(BaseModel):
+    extra_core_cost: int
+    extra_gpu_cost: int
+    extra_ram_cost: int
+    hv_cpu_count: int
+    hv_cpu_ref: str
+    hv_gpu_count: int
+    hv_gpu_ref: str
+    hv_ram: int
+    vm_core: int
+    vm_core_max: int
+    vm_cost: int
+    vm_gpu: int
+    vm_ram: int
+    vm_ram_max: int
+    vm_sku: str
+
+
+class ContextInformation(BaseModel):
+    datacenters: List[DataCenter]
+    user: UserInfo
+    skus: List[SkuInfo]
